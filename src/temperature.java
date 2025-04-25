648449ee4be42abf87f65b36086a01c9a704af8e
suruthi0309 <senthilkumarsruthika@gmail.com>
2025-04-25 10:57:21 +0530

    add temp

---
+++ src/temperature.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class temperature {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        double temp;
+        double newtemp;
+        String unit;
+        System.out.println("Enter the temperature:");
+        temp = scanner.nextDouble();
+        System.out.println("Convert the celsius to fahrenheit? (C or F):");
+        unit =scanner.next().toUpperCase();
+        newtemp=(unit.equals(("C")) ? (temp-32)* 5/9:(temp * 5/9) +32);
+        System.out.printf("%.2f°",newtemp );
+        scanner.close();
+    }
+
+}
